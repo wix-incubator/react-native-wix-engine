b58dfbf7ad8f8ef0577d72fb7c4b86532d1b8080
Hugo Gaio Shiroma <hugo.shiroma@acesso.com>
2021-07-19 14:07:39 -0300

    kotlin version update

--- internal_folder/react-native-wix-engine/ios/ReactNativeWixEngine/AppDelegate.m
+++ internal_folder/react-native-wix-engine/ios/ReactNativeWixEngine/AppDelegate.m
@@ -2,7 +2,6 @@
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
 
 #ifdef FB_SONARKIT_ENABLED
