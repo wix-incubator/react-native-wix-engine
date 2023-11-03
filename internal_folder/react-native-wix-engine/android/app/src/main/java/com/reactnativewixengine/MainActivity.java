1d264cc1089bd3cd55c565146bd538478f0a3fd1
Johan du Toit <johdutoit@shoprite.co.za>
2023-11-03 12:04:28 +0200

    chore: upgrade react native to 0.72.6

--- internal_folder/react-native-wix-engine/android/app/src/main/java/com/reactnativewixengine/MainActivity.java
+++ internal_folder/react-native-wix-engine/android/app/src/main/java/com/reactnativewixengine/MainActivity.java
@@ -1,7 +1,13 @@
 package com.reactnativewixengine;
 
 import com.reactnativenavigation.NavigationActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends NavigationActivity {
 
+  
+
+  
 }
